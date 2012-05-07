3075f06b19fb0a75ce3d880b7aac0a7c05e4a4bc
Daniel Wheeler <daniel.wheeler2@gmail.com>
2012-05-07 17:03:01 +0000

    git-svn-id: svn+ssh://code.matforge.org/wd15/trunk/moffat/electrical@1535 01ab6b12-1d45-0410-8926-c5867bd4e26f

--- electrical/plotSchematic.py
+++ electrical/plotSchematic.py
@@ -1,93 +1,144 @@
 import pylab
 import parameters
 import matplotlib
+from matplotlib.patches import FancyArrowPatch
 
 def plot(filesuffix='.png'):
 
     matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
 
     scale = 1e6
-    d = parameters.delta * scale
-    r = parameters.fieldWidth * scale
+    d = parameters.delta * scale * 0.5
+    r = parameters.fieldWidth * scale * 1.1
     h = parameters.featureDepth * scale
-    w = parameters.trenchWidth * scale
+    w = parameters.trenchWidth * scale * 2
+    L = d + 0.00002 * scale * 2
+    y1 = L + 0.00001 * scale
+    x0 = -0.00004 * scale
+    x1 = 0.00007 * scale
+    y0 = -h - 0.00001 * scale
+
+    eqnFont = 9
+    width = x1 - x0
 
     fig = pylab.figure()
     ax = fig.add_subplot(111, aspect='equal')
 
-    extra = 0
 
-    ax.add_patch(pylab.Rectangle((-40, -h), 200, h + extra, alpha=0.3, color='black', ec='none'))
-    ax.add_patch(pylab.Rectangle((-40, extra), 200, d - extra, alpha=0.2, color='black', ec='none'))
-    ax.add_patch(pylab.Rectangle((-40, d), 200, 20, alpha=0.1, color='black', ec='none'))
-    ax.add_patch(pylab.Rectangle((-40, d + 20), 200, 20, alpha=0.05, color='black', ec='none'))
-    ax.add_patch(pylab.Rectangle((-40, -h - 40), 200, 40, alpha=0.5, color='black', ec='none'))
+    ax.add_patch(pylab.Rectangle((-w / 2, -h), w, h, alpha=0.2, color='black', ec='none', zorder=1))
+    ax.add_patch(pylab.Rectangle((-r / 2, 0), r, d, alpha=0.2, color='black', ec='none', zorder=1))
+    ax.add_patch(pylab.Rectangle((-r / 2, d), r, L - d, alpha=0.1, color='black', ec='none', zorder=1))
+    # ax.add_patch(pylab.Rectangle((x0, L), width, y1 - L, alpha=0.05, color='black', ec='none'))
+    # ax.add_patch(pylab.Rectangle((x0, y0), width, -h - y0, alpha=0.5, color='black', ec='none'))
 
-    pylab.plot((-r / 2, -r / 2, -w / 2, -w / 2, w / 2, w / 2, r / 2, r / 2, -r / 2),
-               (d, 0, 0, -h, -h, 0, 0, d, d), 'k', lw=3, alpha=0.5)
+    pylab.plot((-r / 2, -w / 2, -w / 2, w / 2, w / 2, r / 2),
+               (0, 0, -h, -h, 0, 0), 'k', lw=3, alpha=0.5)
+
+    pylab.plot((-r / 2, -r / 2),
+               (d, 0), 'k--', lw=3, alpha=0.5)
+
+    pylab.plot((r / 2, r / 2),
+               (d, 0), 'k--', lw=3, alpha=0.5)
+
+    pylab.plot((-r / 2, r / 2),
+               (L, L), 'k', lw=3, alpha=0.5)
+
 
     pylab.ion()
-    pylab.xticks((-h / 2, h / 2), (r'$-a_S^{} / P$', r'$a_S^{} / P$'))
-    pylab.yticks((-56, 0, 150, 170), (r'$-h$', 0, r'$\delta$', r'$L$'))
-
-    # ax2 = pylab.twinx(aspect='equal')
-    # pylab.yticks((-56, 0, 150), (r'$-h$', r'0',r'$\delta$'))
-    # pylab.ylim(ymin=-0.00007 * scale)
-    # pylab.ylim(ymax=0.000180 * scale)
-    # pylab.xticks((-0.00004 * scale, 0, 0.00004 * scale))
-    # pylab.xlim(xmax=0.00008 * scale)
-    # pylab.xlim(xmin=-0.00004 * scale)
+    pylab.xticks([])
+    pylab.yticks((-h, 0, d, L), (r'$-h$', 0, r'$\delta$', r'$L$'))
 
     pylab.axes(ax, aspect='equal')
-    pylab.ylim(ymin=-0.00007 * scale)
-    pylab.ylim(ymax=0.000180 * scale)
-    pylab.xlim(xmax=0.00008 * scale)
-    pylab.xlim(xmin=-0.00004 * scale)
-    pylab.xlabel(r'$x$')
-    pylab.ylabel(r'$z$')
+    pylab.ylim(ymin=y0)
+    pylab.ylim(ymax=y1)
+    pylab.xlim(xmax=x1)
+    pylab.xlim(xmin=x0)
+    pylab.xlabel(r'')
+    pylab.ylabel(r'$z$', fontsize=16, rotation='horizontal')
 
-    pylab.text(-25, 70, 
+    pylab.text(-20, d / 2 - 10, 
                 r'\['
                 r'\begin{split}'
-                r'    \partial_z^2 \eta_{\text{Eff}}^{} &= 0 \\'
-                r'    \partial_t C_{\text{Supp}}^{} &= D_{\text{Supp}}^{} \partial_z^2 C_{\text{Supp}}^{} \\'
-                r'    \partial_t C_{\text{Cu}}^{} &= D_{\text{Cu}}^{} \partial_z^2 C_{\text{Cu}}^{}'
+                r'     & \hspace{-25pt} \text{Boundary Layer} \\'
+                r'    \partial_z^2 \eta &= 0 \\'
+                r'    \partial_z^2 C_{\text{Supp}}^{} &= 0 \\'
+                r'    \partial_z^2 C_{\text{Cu}}^{} &= 0'
                 r'\end{split}'
-                r'\]', fontsize=8)
+                r'\]', fontsize=eqnFont)
 
-    pylab.text(-24, -16,
+
+    pylab.text(-2, -20, r'Feature', rotation='vertical', fontsize=eqnFont)
+
+    kX = 15
+    kY = -15
+    pylab.text(kX, kY, r'$k^+ C_{\text{Supp}}^{} \left(1 - \theta\right) = k^- \theta v$', fontsize=eqnFont)
+
+    caseX = 13
+    caseY = -55
+    pylab.text(caseX, caseY,
+                r'\['
+                r'\begin{cases}'
+                r'    & \hspace*{-8pt} \partial_z^2  \eta = \frac{\rho n F}{\Omega} v \frac{P}{a_f}, \\'
+                r'    & \hspace*{-8pt} D_{\text{Supp}}^{} \partial_z^2 C_{\text{Supp}}^{} = \\'
+                r'    & \Gamma k^+ C_{\text{Supp}}^{} \left(1-\theta\right) \frac{P}{a_f},\\'
+                r'    & \hspace*{-8pt} D_{\text{Cu}}^{} \partial_z^2 C_{\text{Cu}}^{} = \frac{v}{\Omega} \frac{P}{a_f}'
+                r'\end{cases}'
+                r'\]', rotation='horizontal', fontsize=eqnFont)
+
+    pylab.text(-28, d - 7, 
+                r'\['
+                r'    C_{\text{Supp}}^{} = C_{\text{Supp}}^{\infty}, C_{\text{Cu}}^{} = C_{\text{Cu}}^{\infty}'
+                r'\]', fontsize=eqnFont)
+
+    pylab.text(-27, L - 8, 
                 r'\['
                 r'\begin{split}'
-                r'    \partial_t \theta =& k^+ C_{\text{Supp}}^{} \left(1 - \theta\right) \\'
-                r'    &- k^- \theta v'
+                r'     & \hspace{-20pt} \text{Reference Electrode} \\'
+                r'     & \eta = E_{\text{Applied}}^{}'
                 r'\end{split}'
-                r'\]', rotation='vertical', fontsize=8)
-    pylab.text(-2, -25, r'Feature', rotation='vertical', fontsize=8)
+                r'\]', fontsize=eqnFont)
 
-    pylab.text(5, -50,
-                r'\['
-                r'\begin{split}'
-                r'    \partial_z^2  \eta_{\text{Eff}}^{} =& \frac{\rho n F}{\Omega} v \Theta \\'
-                r'    \partial_t C_{\text{Supp}}^{} =& \partial_z^2 C_{\text{Supp}}^{} \\'
-                r'    - \Gamma k^+ & C_{\text{Supp}}^{} \left(1-\theta\right)  \Theta\\'
-                r'    \partial_t C_{\text{Cu}}^{} =& D_{\text{Cu}}^{} \partial_z^2 C_{\text{Cu}}^{}  - \frac{v}{\Omega} \Theta '
-                r'\end{split}'
-                r'\]', rotation='horizontal', fontsize=8)
-    pylab.text(-10, 105, r'Electrolyte', fontsize=8)
+    y910 = -15
 
-    pylab.text(-27, 152, 
-                r'$C_{\text{Supp}}^{} = C_{\text{Supp}}^{\infty}, C_{\text{Cu}}^{} = C_{\text{Cu}}^{\infty}$', fontsize=8)
-    pylab.text(-10, 162, r'Boundary Layer', fontsize=8)
+    pylab.text(-r / 2 - 3, y910, r'Eqs. (9, 10)', fontsize=eqnFont) 
+    ax.add_patch(FancyArrowPatch((-r / 4 - 5, y910 + 3.5),(-r / 4 + 5, 1.5), arrowstyle='->',mutation_scale=10, lw=1))
 
-    pylab.text(-27, 172, 
-                r'$\eta_{\text{Eff}}^{} = E_{\text{Applied}}^{}$', fontsize=8)
-    pylab.text(15, 173, r'Reference Electrode', fontsize=8)
+    y1112 = -h + 15
+    pylab.text(-r / 2 - 3, y1112, r'Eqs. (11, 12)', fontsize=eqnFont) 
+    ax.add_patch(FancyArrowPatch((-r / 4 - 5, y1112),(0, -h-0.8), arrowstyle='->',mutation_scale=10, lw=1))
 
-    pylab.text(-30, -68, r'$\Theta \left(z\right)=\frac{P}{a_F^{}} + \delta \left(z\right) \left(\frac{a_S^{}}{a_F^{}} - 1\right) + \delta\left(z+h\right)$', fontsize=8)
-    ##pylab.text(57, 87, r'$L$', fontsize=8)
 
-    ##from matplotlib.patches import FancyArrowPatch
-    ##ax.add_patch(FancyArrowPatch((55, -3),(55, 175),arrowstyle='<->', lw=2, mutation_scale=20))
+    afy = -h + h * 0.8
+    afw = w * 1.1
+    ax.add_patch(FancyArrowPatch((-afw / 2, afy),(afw / 2, afy), arrowstyle='<->',mutation_scale=10, lw=1))
+    pylab.text(-2, afy + 2, r'$a_f$', fontsize=eqnFont) 
+
+    asy = 0 + d * 0.1
+    asw = r * 1.02
+    ax.add_patch(FancyArrowPatch((-asw / 2, asy),(asw / 2, asy), arrowstyle='<->',mutation_scale=15, lw=1))
+    pylab.text(-2, asy + 2, r'$a_s$', fontsize=eqnFont)
+
+    caseYY = caseY + 16
+    caseYYY = caseYY - 8
+    ax.add_patch(FancyArrowPatch((caseX, caseYY),(-1, caseYYY), arrowstyle='->',mutation_scale=10, lw=1))
+
+    kYY = kY + 3
+    kYYY = kYY + 4
+    ax.add_patch(FancyArrowPatch((kX, kYY),(w / 2 - 1.5, kYYY), arrowstyle='->',mutation_scale=10, lw=1))
+
+    kkYY = kY + 3
+    ax.add_patch(FancyArrowPatch((kX, kkYY),(w / 2 + 4, 1.5), arrowstyle='->',mutation_scale=10, lw=1))
+
+    ## break in axes
+    breakX = d + (L - d) / 2
+    hX = 10
+    ls = 10
+    diag = 5
+    ax.add_patch(pylab.Rectangle((x0, breakX - hX / 2), x1 - x0 + 0.0, hX, alpha=1.0, color='white', ec='white', zorder=100, clip_on=False))
+    ax.plot((x0 - ls / 2, x0 + ls / 2), (breakX - hX / 2 + diag / 2, breakX - hX / 2 - diag / 2), 'k', clip_on=False, zorder=101)
+    ax.plot((x0 - ls / 2, x0 + ls / 2), (breakX + hX / 2 + diag / 2, breakX + hX / 2 - diag / 2), 'k', clip_on=False, zorder=101)
+    ax.plot((x1 - ls / 2, x1 + ls / 2), (breakX - hX / 2 + diag / 2, breakX - hX / 2 - diag / 2), 'k', clip_on=False, zorder=101)
+    ax.plot((x1 - ls / 2, x1 + ls / 2), (breakX + hX / 2 + diag / 2, breakX + hX / 2 - diag / 2), 'k', clip_on=False, zorder=101)
 
     pylab.savefig('schematic' + filesuffix)
 
